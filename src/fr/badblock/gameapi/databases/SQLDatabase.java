dc74b0645e091558688d1d8f2594b6f17dd31f82
xMalware <Aurelian User@Aurelian>
2016-06-01 01:06:29 +0200

    SQL As A MultiThreaded Techno, can it be real ? :o

--- src/fr/badblock/gameapi/databases/SQLDatabase.java
+++ src/fr/badblock/gameapi/databases/SQLDatabase.java
@@ -8,9 +8,10 @@ import fr.badblock.gameapi.utils.general.Callback;
 
 /**
  * Représente la base de donnée SQL
- * @author LeLanN
+ * @author LeLanN/xMalware
  */
 public interface SQLDatabase {
+	
 	/**
 	 * Récupère un statement pour une gestion plus personnalisée du SQL. Ne pas oublier de le close :0
 	 * @return Le statement
@@ -24,23 +25,46 @@ public interface SQLDatabase {
 	public PreparedStatement preparedStatement(String request) throws Exception;
 	
 	/**
-	 * Envoit une requête de query
+	 * Envoi une requête de query
 	 * @param request La requête
 	 * @return La réponse
+	 * @deprecated please use request(String, SQLRequestType) instead of that not correctly managed multithreading
 	 */
 	public ResultSet query(String request) throws Exception;
 	
 	/**
-	 * Envoit une requête de query
+	 * Envoi une requête de query
 	 * @param request La requête
 	 * @param callback Le callback pour la réponse
+	 * @deprecated please use request(String, SQLRequestType) instead of that not correctly managed multithreading
 	 */
 	public void queryAsynchronously(String request, Callback<ResultSet> callback);
 	
 	/**
-	 * Envoit une requête d'update
+	 * Envoi une requête d'update
 	 * @param request La requ$ete
 	 * @param synchronously Si la requête doit être fait de manière synchrone
+	 * @deprecated please use call(String, SQLRequestType) instead of that not correctly managed multithreading
 	 */
 	public void update(String request, boolean synchronously) throws Exception;
+
+	/**
+	 * Appeler un gestionnaire Multithreading qui va gérer en asynchrone la requête
+	 * La requête peut être donc traitée de sorte qu'elle ne renvoit aucune information
+	 * ou qu'elle renvoit des données, se référer à {@link fr.badblock.gameapi.databases.SQLRequestType}
+	 * @param request > Requête
+	 * @param sqlRequestType > Type de la requête, DATA OR QUERY
+	 * @param callback > Réponse de la requête
+	 */
+	public void call(String request, SQLRequestType sqlRequestType, Callback<ResultSet> callback);
+	
+	/**
+	 * Appeler un gestionnaire Multithreading qui va gérer en asynchrone la requête
+	 * La requête peut être donc traitée de sorte qu'elle ne renvoit aucune information
+	 * ou qu'elle renvoit des données, se référer à {@link fr.badblock.gameapi.databases.SQLRequestType}
+	 * @param request > Requête
+	 * @param sqlRequestType > Type de la requête, DATA OR QUERY
+	 */
+	public void call(String request, SQLRequestType sqlRequestType);
+	
 }
---
+++ src/fr/badblock/gameapi/databases/SQLRequestType.java
@@ -0,0 +1,8 @@
+package fr.badblock.gameapi.databases;
+
+public enum SQLRequestType {
+
+	UPDATE,
+	QUERY;
+	
+}
