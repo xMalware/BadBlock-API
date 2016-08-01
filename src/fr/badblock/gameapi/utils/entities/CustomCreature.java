b5bdaa46927efc39b6d59845c23f881570e896ce
LeLanN <audrandoublet@gmail.com>
2016-08-01 19:27:23 +0200

    New entities

--- src/fr/badblock/gameapi/utils/entities/CustomCreature.java
+++ src/fr/badblock/gameapi/utils/entities/CustomCreature.java
@@ -1,6 +1,7 @@
 package fr.badblock.gameapi.utils.entities;
 
-import org.bukkit.Location;
+import java.util.List;
+
 import org.bukkit.entity.Entity;
 
 /**
@@ -18,128 +19,221 @@ public interface CustomCreature {
 	 * @return Le type de la créature.
 	 */
 	public CreatureType getEntityType();
-	
-	/**
-	 * Vérifie si la créature est sur le sol
-	 */
-	public boolean isOnGround();
-	
-	/**
-	 * Vérifie si la créature est dans la lave
-	 */
-	public boolean isInLava();
-	
-	/**
-	 * Vérifie si la créature est dans l'eau
-	 */
-	public boolean isInWater();
 
-	/**
-	 * Vérifie si la créature est résistante au feu
-	 */
-	public boolean isFireProof();
-	
 	/**
 	 * Définit si l'entité est invisible
 	 * @param invisible Si elle est invisible
+	 * @deprecated Utiliser {@link #addCreatureFlag(CreatureFlag)} et {@link #removeCreatureFlag(CreatureFlag)} à la place
 	 */
-	public void   setInvisible(boolean invisible);
-	
+	default void setInvisible(boolean invisible){
+		if(invisible)
+			addCreatureFlag(CreatureFlag.INVISIBLE);
+		else removeCreatureFlag(CreatureFlag.INVISIBLE);
+	}
+
+	/**
+	 * Vérifie si l'entité est invisible
+	 * @deprecated Utiliser {@link #hasCreatureFlag(CreatureFlag)} à la place
+	 */
+	default boolean isInvisible(){
+		return hasCreatureFlag(CreatureFlag.INVISIBLE);
+	}
+
+	/**
+	 * Vérifie si la créature est résistante au feu
+	 * @deprecated Utiliser {@link #hasCreatureFlag(CreatureFlag)} à la place
+	 */
+	default boolean isFireProof(){
+		return hasCreatureFlag(CreatureFlag.FIREPROOF);
+	}
+
 	/**
 	 * Défini si la créature est résistante au feu
 	 * @param fireProof Sa résistance (ou non)
+	 * @deprecated Utiliser {@link #addCreatureFlag(CreatureFlag)} et {@link #removeCreatureFlag(CreatureFlag)} à la place
 	 */
-	public void    setFireProof(boolean fireProof);
-	
+	default void setFireProof(boolean fireProof){
+		if(fireProof)
+			addCreatureFlag(CreatureFlag.FIREPROOF);
+		else removeCreatureFlag(CreatureFlag.FIREPROOF);
+	}
+
 	/**
 	 * Vérifie si l'entité est invincible (ne peut prendre aucun dégat naturels, seul setHealth lui fera perdre de la vie)
+	 * @deprecated Utiliser {@link #hasCreatureFlag(CreatureFlag)} à la place
 	 */
-	public boolean isInvincible();
+	default boolean isInvincible(){
+		return hasCreatureFlag(CreatureFlag.INVINCIBLE);
+	}
 
 	/**
 	 * Défini si la créature est invincible (ne peut prendre aucun dégats naturels, seul setHealth lui fera perdre de la vie) 
 	 * @param invincible Si la créature est invincible
+	 * @deprecated Utiliser {@link #addCreatureFlag(CreatureFlag)} et {@link #removeCreatureFlag(CreatureFlag)} à la place
 	 */
-	public void    setInvincible(boolean invincible);
-	
-	/**
-	 * Change la vitesse de l'entité
-	 * @param speed La nouvelle vitesse
-	 */
-	public void    setSpeed(double speed);
-	
-	/**
-	 * Récupère la vitesse de l'entité
-	 */
-	public double  getSpeed();
-	
-	/**
-	 * Change les dégats fait par l'entité
-	 * @param damage Les nouveaux dégats
-	 */
-	public void    setDamage(double damage);
-	
-	/**
-	 * Récupère les dégats fait par l'entité
-	 */
-	public double  getDamage();
-	
-	/**
-	 * Vérifie si l'entité peut se déplacer. Si non, elle n'est plus soumise à la gravité, n'a pas de knockback en combat, ...
-	 */
-	public boolean isMovable();
-	
-	/**
-	 * Autorise ou non l'entité à se déplacer
-	 * @param movable Si l'entité peut se déplacer
-	 */
-	public void    setMovable(boolean movable);
-	
+	default void setInvincible(boolean invincible){
+		if(invincible)
+			addCreatureFlag(CreatureFlag.INVINCIBLE);
+		else removeCreatureFlag(CreatureFlag.INVINCIBLE);
+	}
+
 	/**
 	 * Vérifie si l'entité est agressive (va essayer de frapper un joueur si elle en voit un)
+	 * @deprecated Utiliser {@link #hasCreatureFlag(CreatureFlag)} à la place
 	 */
-	public boolean isAgressive();
+	default boolean isAgressive(){
+		return hasCreatureFlag(CreatureFlag.AGRESSIVE);
+	}
 
 	/**
 	 * Défini si l'entité est agressive (va essayer de frapper un joueur si elle en voit un).
 	 * @param agressive Si l'entité est agressive.
+	 * @deprecated Utiliser {@link #addCreatureFlag(CreatureFlag)} et {@link #removeCreatureFlag(CreatureFlag)} à la place
 	 */
-	public void    setAgressive(boolean agressive);
-	
+	default void setAgressive(boolean agressive){
+		if(agressive)
+			addCreatureFlag(CreatureFlag.AGRESSIVE);
+		else removeCreatureFlag(CreatureFlag.AGRESSIVE);
+	}
+
 	/**
 	 * Vérifie si l'entité peut se faire monter (clique droit sur l'entité) par n'importe quel joueur.
 	 * Pour le cochon, il ne nécessite plus de selle et de canne.
+	 * @deprecated Utiliser {@link #hasCreatureFlag(CreatureFlag)} à la place
 	 */
-	public boolean isRideable();
+	default boolean isRideable(){
+		return hasCreatureFlag(CreatureFlag.RIDEABLE);
+	}
 
 	/**
 	 * Défini si l'entité peut se faire monter (clique droit sur l'entité) par n'importe quel joueur.
 	 * @param rideable Si l'entité peut se faire monter
+	 * @deprecated Utiliser {@link #addCreatureFlag(CreatureFlag)} et {@link #removeCreatureFlag(CreatureFlag)} à la place
 	 */
-	public void    setRideable(boolean rideable);
-	
+	default void setRideable(boolean rideable){
+		if(rideable)
+			addCreatureFlag(CreatureFlag.RIDEABLE);
+		else removeCreatureFlag(CreatureFlag.RIDEABLE);
+	}
+
+	/**
+	 * Change la vitesse de l'entité
+	 * @param speed La nouvelle vitesse
+	 * @deprecated Utiliser {@link #setCreatureGenericAttribute(CreatureGenericAttribute, double)}
+	 */
+	default void setSpeed(double speed){
+		setCreatureGenericAttribute(CreatureGenericAttribute.SPEED, speed);
+	}
+
+	/**
+	 * Récupère la vitesse de l'entité
+	 * @deprecated Utiliser {@link #getCreatureGenericAttribute(CreatureGenericAttribute)}
+	 */
+	default double getSpeed(){
+		return getCreatureGenericAttribute(CreatureGenericAttribute.SPEED);
+	}
+
+	/**
+	 * Change les dégats fait par l'entité
+	 * @param damage Les nouveaux dégats
+	 * @deprecated Utiliser {@link #setCreatureGenericAttribute(CreatureGenericAttribute, double)}
+	 */
+	default void setDamage(double damage){
+		setCreatureGenericAttribute(CreatureGenericAttribute.DAMAGE, damage);
+	}
+
+	/**
+	 * Récupère les dégats fait par l'entité
+	 * @deprecated Utiliser {@link #getCreatureGenericAttribute(CreatureGenericAttribute)}
+	 */
+	default double getDamage(){
+		return getCreatureGenericAttribute(CreatureGenericAttribute.DAMAGE);
+	}
+
+	/**
+	 * Vérifie si l'entité peut se déplacer. Si non, elle n'est plus soumise à la gravité, n'a pas de knockback en combat, ...
+	 * @deprecated Utiliser {@link #getCreatureBehaviour(CreatureBehaviour)}
+	 */
+	default boolean isMovable() {
+		return getCreatureBehaviour() == CreatureBehaviour.MOTIONLESS;
+	}
+
+	/**
+	 * Autorise ou non l'entité à se déplacer
+	 * @param movable Si l'entité peut se déplacer
+	 * @deprecated Utiliser {@link #setCreatureBehaviour(CreatureBehaviour)}
+	 */
+	default void setMovable(boolean movable) {
+		setCreatureBehaviour(movable ? CreatureBehaviour.NORMAL : CreatureBehaviour.MOTIONLESS);
+	}
+
 	/**
 	 * Vérifie si l'entité peut voler. Une entité non montée qui vole ne va pas attaquer les joueurs et va se contenter de se déplacer rapidement (à la manière d'une chauve-souris).
+	 * @deprecated Utiliser {@link #getCreatureBehaviour(CreatureBehaviour)}
 	 */
-	public boolean isAllowedToFly();
+	default boolean isAllowedToFly(){
+		return getCreatureBehaviour() == CreatureBehaviour.FLYING;
+	}
 
 	/**
 	 * Défini si l'entité peut voler. Une entité non montée qui vole ne va pas attaquer les joueurs et va se contenter de se déplacer rapidement (à la manière d'une chauve-souris).
 	 * @param allowed Si l'entité peut voler.
+	 * @deprecated Utiliser {@link #setCreatureBehaviour(CreatureBehaviour)}
 	 */
-	public void    setAllowedToFly(boolean allowed);
+	default void setAllowedToFly(boolean allowed) {
+		setCreatureBehaviour(allowed ? CreatureBehaviour.NORMAL : CreatureBehaviour.FLYING);
+	}
+
+	public void setCreatureBehaviour(CreatureBehaviour behaviour);
+
+	public CreatureBehaviour getCreatureBehaviour();
+
+	default void addCreatureFlag(CreatureFlag flag){
+		if(!getFlags().contains(flag)){
+			getFlags().add(flag);
+			regenerateAttributes();
+		}
+	}
+
+	default void removeCreatureFlag(CreatureFlag flag){
+		getFlags().remove(flag);
+		regenerateAttributes();
+	}
+
+	default boolean hasCreatureFlag(CreatureFlag flag){
+		return getFlags().contains(flag);
+	}
+
+	public List<CreatureFlag> getFlags();
+
+	public void setCreatureGenericAttribute(CreatureGenericAttribute attribute, double value);
 	
-	/**
-	 * Fait exploser l'entité
-	 * @param power La puissance de l'explosion
-	 * @param flaming Si l'explosion provoque des flammes
-	 * @param smoking Si l'explosion provoque de la fumée
-	 */
-	public void    explode(Location location, float power, boolean flaming, boolean smoking);
+	public double getCreatureGenericAttribute(CreatureGenericAttribute attribute);
 	
+	public void regenerateAttributes();
+
 	/**
 	 * Récupère l'entité Bukkit
 	 * @return L'entité Bukkit
 	 */
 	public Entity  getBukkit();
+
+	public enum CreatureFlag {
+		RIDEABLE,
+		AGRESSIVE,
+		INVINCIBLE,
+		FIREPROOF,
+		INVISIBLE;
+	}
+
+	public enum CreatureBehaviour {
+		MOTIONLESS,
+		FLYING,
+		NORMAL;
+	}
+	
+	public enum CreatureGenericAttribute {
+		SPEED,
+		DAMAGE;
+	}
 }
