7feec5ae8e78a504c067bb728d71a767b24c4fd0
LeLanN <audrandoublet@gmail.com>
2016-08-03 11:58:13 +0200

    Adding examples for disguises

---
+++ src/fr/badblock/example/gameapi/CustomDisguiseExample.java
@@ -0,0 +1,33 @@
+package fr.badblock.example.gameapi;
+
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+
+import fr.badblock.gameapi.GameAPI;
+import fr.badblock.gameapi.disguise.Disguise;
+import fr.badblock.gameapi.disguise.FallingBlockDisguise;
+import fr.badblock.gameapi.fakeentities.FakeEntity;
+import fr.badblock.gameapi.players.BadblockPlayer;
+import fr.badblock.gameapi.utils.i18n.TranslatableString;
+import lombok.Getter;
+import lombok.Setter;
+
+/** 
+ * The same as {@link FallingBlockDisguise}, already exist in API 
+ * @author LeLanN
+ */
+public class CustomDisguiseExample extends Disguise {
+	@Getter@Setter
+	private Material type;
+	
+	public CustomDisguiseExample(TranslatableString customName, boolean doWithScoreboard, boolean canSeeHimself, Material type) {
+		super(EntityType.FALLING_BLOCK, customName, doWithScoreboard, canSeeHimself);
+		
+		this.type = type;
+	}
+	
+	@Override
+	public FakeEntity<?> createFakeEntity(BadblockPlayer player){
+		return GameAPI.getAPI().spawnFakeFallingBlock(player.getLocation(), type, (byte) 0);
+	}
+}
---
+++ src/fr/badblock/example/gameapi/DisguiseExample.java
@@ -0,0 +1,28 @@
+package fr.badblock.example.gameapi;
+
+import org.bukkit.entity.EntityType;
+
+import fr.badblock.gameapi.disguise.Disguise;
+import fr.badblock.gameapi.players.BadblockPlayer;
+
+/**
+ * Un exemple de l'utilisation des déguisements
+ * @author LeLanN
+ */
+public class DisguiseExample {
+	private BadblockPlayer player;
+	private Disguise	   disguise;
+	
+	public DisguiseExample(BadblockPlayer player){
+		this.player   = player;
+		this.disguise = new Disguise(EntityType.ZOMBIE, null, true, true);
+	}
+	
+	public void disguise(){
+		player.disguise(disguise);
+	}
+	
+	public void undisguise(){
+		player.undisguise();
+	}
+}
