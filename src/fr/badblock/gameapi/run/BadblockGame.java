c886a4e8c101e3eb07245b78602923e57a21f320
LeLanN <audrandoublet@gmail.com>
2016-08-11 00:17:41 +0200

    Adding game informations

---
+++ src/fr/badblock/gameapi/run/BadblockGame.java
@@ -0,0 +1,45 @@
+package fr.badblock.gameapi.run;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import fr.badblock.gameapi.GameAPI;
+import fr.badblock.gameapi.players.BadblockPlayer;
+import fr.badblock.gameapi.utils.i18n.Locale;
+import fr.badblock.gameapi.utils.itemstack.ItemStackFactory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public enum BadblockGame {
+	RUSH("rush", "Rush", GameAPI.getAPI().createItemStackFactory().type(Material.BED));
+	
+	private final String 		   internalGameName,
+								   displayGameName;
+	private final ItemStackFactory itemStackFactory;
+	
+	@Setter
+	private BadblockGameData gameData;
+	
+	BadblockGame(String internalGameName, String displayGameName, ItemStackFactory itemStackFactory){
+		itemStackFactory.displayName("gameitems." + internalGameName + ".displayname");
+		itemStackFactory.lore("gameitems." + internalGameName + ".lore");
+		
+		this.internalGameName = internalGameName;
+		this.displayGameName  = displayGameName;
+		this.itemStackFactory = itemStackFactory;
+	}
+	
+	public ItemStack createItemStack(Locale locale){
+		return itemStackFactory.clone().doWithI18n(locale).create(1);
+	}
+	
+	public ItemStack createItemStack(BadblockPlayer player){
+		return createItemStack(player.getPlayerData().getLocale());
+	}
+	
+	public void use(){
+		GameAPI.setGameName(displayGameName);
+		GameAPI.setInternalGameName(internalGameName);
+	}
+}
---
+++ src/fr/badblock/gameapi/run/BadblockGameData.java
@@ -0,0 +1,7 @@
+package fr.badblock.gameapi.run;
+
+import fr.badblock.gameapi.achievements.AchievementList;
+
+public interface BadblockGameData {
+	public AchievementList getAchievements();
+}
