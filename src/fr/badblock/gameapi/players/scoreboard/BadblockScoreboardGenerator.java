65130791f1c9201a063750c77cbd6f531c46da41
LeLanN <audrandoublet@gmail.com>
2016-08-25 13:50:42 +0200

    Add scoreboard footer in API

--- src/fr/badblock/gameapi/players/scoreboard/BadblockScoreboardGenerator.java
+++ src/fr/badblock/gameapi/players/scoreboard/BadblockScoreboardGenerator.java
@@ -1,12 +1,46 @@
 package fr.badblock.gameapi.players.scoreboard;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import fr.badblock.gameapi.GameAPI;
+
 /**
  * Représente la classe permettant de générer le scoreboard.
  * @author LeLanN
  */
-public interface BadblockScoreboardGenerator {
+public abstract class BadblockScoreboardGenerator {
+	private int 			   current	 = 0;
+	private CustomObjective	   objective;
+	
 	/**
 	 * Génère le scoreboard
 	 */
-	public void generate();
+	public abstract void generate();
+	
+	/**
+	 * Permet de générer une dernière ligne animée
+	 * @param objective L'objectif géré
+	 */
+	public void doBadblockFooter(CustomObjective objective){
+		this.objective = objective;
+		Bukkit.getScheduler().runTaskTimer(GameAPI.getAPI(), this::doBadblockFooter0, 0, 2L);
+	}
+	
+	private void doBadblockFooter0(){
+		String base = "/badblock";
+
+		if(current < 0){
+			objective.changeLine(1, "&b" + base);
+		} else {
+			String result = "&b" + base.substring(0, current) + ChatColor.BLUE + base.substring(current, current + 1) + "&b" + base.substring(current + 1);
+			objective.changeLine(1, result);
+		}
+		
+		current++;
+		
+		if(current == base.length()){
+			current = -10;
+		}
+	}
 }
