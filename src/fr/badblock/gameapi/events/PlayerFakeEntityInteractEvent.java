5b28925ea95e0755b6732dcd3e6c61a3965c8548
LeLanN <audrandoublet@gmail.com>
2016-08-26 21:13:25 +0200

    a

--- src/fr/badblock/gameapi/events/PlayerFakeEntityInteractEvent.java
+++ src/fr/badblock/gameapi/events/PlayerFakeEntityInteractEvent.java
@@ -1,5 +1,6 @@
 package fr.badblock.gameapi.events;
 
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
@@ -8,18 +9,21 @@ import fr.badblock.gameapi.packets.in.play.PlayInUseEntity.UseEntityAction;
 import fr.badblock.gameapi.players.BadblockPlayer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Appelée lorsque un joueur clique sur un fausse entité.
  * @author LeLanN
  */
 @AllArgsConstructor
-public class PlayerFakeEntityInteractEvent extends Event {
+public class PlayerFakeEntityInteractEvent extends Event implements Cancellable {
 	private static final HandlerList	handlers	= new HandlerList();
 
 	@Getter private BadblockPlayer  player;
 	@Getter private FakeEntity<?>   entity;
 	@Getter private UseEntityAction action;
+	@Getter@Setter
+			private boolean			cancelled;
 	
 	public HandlerList getHandlers() {
 		return handlers;
