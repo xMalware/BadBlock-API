ac20c015b109f4c23ff5b6e7dbf30ea1a5951354
LeLanN <audrandoublet@gmail.com>
2016-08-31 19:59:55 +0200

    OupOup

--- src/fr/badblock/gameapi/utils/ServerProperties.java
+++ src/fr/badblock/gameapi/utils/ServerProperties.java
@@ -6,10 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-import lombok.Getter;
-
+/**
+ * Classe utilitaire permettant de récupérer et définir facilement des données configurées dans le server.properties
+ * @author LeLanN
+ */
 public class ServerProperties {
-	@Getter
 	private final static Properties properties;
 	private final static File		propertiesFile = new File("server.properties");
 	static {
@@ -17,26 +18,28 @@ public class ServerProperties {
 
 		try {
 			properties.load(new FileInputStream(propertiesFile));
-			/*Files.readAllLines(Paths.get(propertiesFile.getAbsolutePath()), Charset.defaultCharset()).forEach(line -> {
-				String[] splitted     = line.split("=");
-				String   propertyName = splitted[0];
-				String   property     = splitted.length == 1 ? "" : StringUtils.join(splitted, "=", 1);
-				
-				properties.setProperty(propertyName, property);
-			})*/;
 		} catch (IOException e) {
-			System.out.println("Can not read server.properties file :");
+			System.out.println("Can not read " + propertiesFile.getName() + " file :");
 			e.printStackTrace();
 		}
-
-
 	}
 
+	/**
+	 * Récupère les données
+	 * @return {@link Properties}
+	 */
+	public static Properties getProperties(){
+		return properties;
+	}
+	
+	/**
+	 * Sauvegarde les données
+	 */
 	public static void saveProperties(){
 		try {
 			properties.store(new FileOutputStream(propertiesFile), "");
 		} catch (IOException e) {
-			System.out.println("Can not save server.properties file :");
+			System.out.println("Can not save " + propertiesFile.getName() +  "file :");
 			e.printStackTrace();
 		}
 	}
