bb65198c0972583f3c0dfec52d31f939fb5f9f24
LeLanN <audrandoublet@gmail.com>
2016-09-17 13:47:32 +0200

    javadoc

--- src/fr/badblock/gameapi/players/BadblockPlayer.java
+++ src/fr/badblock/gameapi/players/BadblockPlayer.java
@@ -224,7 +224,12 @@ public interface BadblockPlayer extends Player, BadblockPlayerData {
 	 */
 	public void jailPlayerAt(Location location);
 
-	
+	/**
+	 * Lance un projectile custom, qui appelera une méthode lorsqu'il touchera un block ou une entité
+	 * @param projectile La classe du projectile
+	 * @param action L'action
+	 * @return Le projectile
+	 */
 	public <T extends Projectile> T launchProjectile(Class<T> projectile, BiConsumer<Block, Entity> action);
 	
 	/**
