403f5cbedde47a62e049e2a141ad8147a736d34c
LeLanN <audrandoublet@gmail.com>
2016-10-08 15:54:33 +0200

    implement NamedEntitySpawn packet

--- src/fr/badblock/gameapi/packets/out/play/PlayNamedEntitySpawn.java
+++ src/fr/badblock/gameapi/packets/out/play/PlayNamedEntitySpawn.java
@@ -1,8 +1,12 @@
 package fr.badblock.gameapi.packets.out.play;
 
-import org.bukkit.entity.Player;
+import java.util.UUID;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
 
 import fr.badblock.gameapi.packets.BadblockOutPacket;
+import fr.badblock.gameapi.packets.watchers.WatcherEntity;
 
 /**
  * Packet envoyé au joueur quand un autre joueur entre dans son champs de vision
@@ -11,18 +15,83 @@ import fr.badblock.gameapi.packets.BadblockOutPacket;
  */
 public interface PlayNamedEntitySpawn extends BadblockOutPacket {
 	/**
-	 * Récupère le joueur qui doit être affiché
+	 * Récupère l'ID de l'entité
 	 * 
-	 * @return Le joueur
+	 * @return L'ID
 	 */
-	public Player getPlayer();
+	public int getEntityId();
 
 	/**
-	 * Définit le joueur qui doit être affiché
+	 * Récupère la position
 	 * 
-	 * @param player
-	 *            Le joueur
+	 * @return La position
+	 */
+	public Location getLocation();
+
+	/**
+	 * Récupère l'UUID
+	 * 
+	 * @return L'UUID
+	 */
+	public UUID getUniqueId();
+	
+	/**
+	 * Récupère le type d'objet dans la main
+	 * @return Le type
+	 */
+	public Material getItemInHand();
+
+	/**
+	 * Récupère les watchers de l'entité
+	 * 
+	 * @return Les watchers
+	 */
+	public WatcherEntity getWatchers();
+
+	/**
+	 * Définit l'ID de l'entité
+	 * 
+	 * @param id
+	 *            L'ID
 	 * @return Le packet
 	 */
-	public PlayNamedEntitySpawn setPlayer(Player player);
+	public PlayNamedEntitySpawn setEntityId(int id);
+
+	/**
+	 * Définit la position
+	 * 
+	 * @param location
+	 *            La position
+	 * @return Le packet
+	 */
+	public PlayNamedEntitySpawn setLocation(Location location);
+
+	/**
+	 * Définit l'UUID
+	 * 
+	 * @param uniqueId
+	 *            L'UUID
+	 * @return Le packet
+	 */
+	public PlayNamedEntitySpawn setUniqueId(UUID uniqueId);
+
+	/**
+	 * Définit le type d'objet dans la main
+	 * 
+	 * @param type
+	 *            Le type
+	 * @return Le packet
+	 */
+	public PlayNamedEntitySpawn setItemInHand(Material type);
+
+	
+	/**
+	 * Définit les watchers de l'entité
+	 * 
+	 * @param watcher
+	 *            Les watchers
+	 * @return Le packet
+	 */
+	public PlayNamedEntitySpawn setWatchers(WatcherEntity watcher);
+
 }
