bb9f3b8d59b99c30d4c03beedf44127d46538ac0
xMalware <xmalware2@gmail.com>
2017-01-31 23:15:05 +0100

    Fix

--- src/fr/badblock/gameapi/players/data/boosters/Booster.java
+++ src/fr/badblock/gameapi/players/data/boosters/Booster.java
@@ -10,6 +10,8 @@ public class Booster {
 	private int    id;
 	private double coinsMultiplier;
 	private double xpMultiplier;
+	private double minRandomBadcoins;
+	private double maxRandomBadcoins;
 	private double maxBadcoins;
 	private double maxXP;
 	private long   length;
