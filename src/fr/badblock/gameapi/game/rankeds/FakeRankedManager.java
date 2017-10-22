fe3aeb5c3e79ce3c3f2125956a31f845ed88d7f3
xMalware <xmalware2@gmail.com>
2017-10-23 01:02:09 +0200

    Edited BadblockGame ranked manager!

--- src/fr/badblock/gameapi/game/rankeds/FakeRankedManager.java
+++ src/fr/badblock/gameapi/game/rankeds/FakeRankedManager.java
@@ -1,19 +1,18 @@
 package fr.badblock.gameapi.game.rankeds;
 
 import fr.badblock.gameapi.players.BadblockPlayer;
-import fr.badblock.gameapi.run.BadblockGame;
 
 public class FakeRankedManager extends RankedManager
 {
 
 	@Override
-	public void initialize(BadblockGame badblockGame, String... fields) {
-		System.out.println("[FakeRankedManager] (" +badblockGame.getDisplayGameName() + ") Initialize with " + fields.length + " data.");
+	public void initialize(String gameName, String... fields) {
+		System.out.println("[FakeRankedManager] (" + gameName + ") Initialize with " + fields.length + " data.");
 	}
 
 	@Override
-	public void fill(BadblockGame badblockGame, BadblockPlayer badblockPlayer, long... data) {
-		System.out.println("[FakeRankedManager] (" +badblockGame.getDisplayGameName() + ") Fill " + badblockPlayer.getName() + " with " + data.length + " data.");
+	public void fill(String gameName, BadblockPlayer badblockPlayer, long... data) {
+		System.out.println("[FakeRankedManager] (" + gameName + ") Fill " + badblockPlayer.getName() + " with " + data.length + " data.");
 	}
 	
 }
--- src/fr/badblock/gameapi/game/rankeds/RankedManager.java
+++ src/fr/badblock/gameapi/game/rankeds/RankedManager.java
@@ -1,13 +1,12 @@
 package fr.badblock.gameapi.game.rankeds;
 
 import fr.badblock.gameapi.players.BadblockPlayer;
-import fr.badblock.gameapi.run.BadblockGame;
 
 public abstract class RankedManager {
 
 	public static RankedManager instance = new FakeRankedManager();
 
-	public abstract void initialize(BadblockGame badblockGame, String... fields);
-	public abstract void fill(BadblockGame badblockGame, BadblockPlayer badblockPlayer, long... data);
+	public abstract void initialize(String gameName, String... fields);
+	public abstract void fill(String gameName, BadblockPlayer badblockPlayer, long... data);
 
 }
