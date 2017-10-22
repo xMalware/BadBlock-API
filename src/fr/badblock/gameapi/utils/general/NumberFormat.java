888064ec92caf7b9db508d08402f8b39a4468645
xMalware <xmalware2@gmail.com>
2017-10-22 12:26:14 +0200

    Added number formatter

---
+++ src/fr/badblock/gameapi/utils/general/NumberFormat.java
@@ -0,0 +1,33 @@
+package fr.badblock.gameapi.utils.general;
+
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NumberFormat {
+
+	private static final NavigableMap<Long, String> suffixes = new TreeMap<> ();
+	static {
+	  suffixes.put(1_000L, "k");
+	  suffixes.put(1_000_000L, "M");
+	  suffixes.put(1_000_000_000L, "G");
+	  suffixes.put(1_000_000_000_000L, "T");
+	  suffixes.put(1_000_000_000_000_000L, "P");
+	  suffixes.put(1_000_000_000_000_000_000L, "E");
+	}
+
+	public static String format(long value) {
+	  if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1);
+	  if (value < 0) return "-" + format(-value);
+	  if (value < 1000) return Long.toString(value);
+	  
+	  Entry<Long, String> e = suffixes.floorEntry(value);
+	  Long divideBy = e.getKey();
+	  String suffix = e.getValue();
+
+	  long truncated = value / (divideBy / 10);
+	  boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
+	  return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
+	}
+	
+}
