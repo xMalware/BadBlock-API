2bdc86303e29df67811580a5fbdf31a2c3bf6708
xMalware <xmalware2@gmail.com>
2017-10-23 03:18:49 +0200

    f

--- src/fr/badblock/gameapi/game/rankeds/RankedManager.java
+++ src/fr/badblock/gameapi/game/rankeds/RankedManager.java
@@ -9,7 +9,7 @@ public abstract class RankedManager {
 	public abstract String getCurrentRankedGameName();
 	public abstract void initialize(String gameName, String... fields);
 	public abstract void fill(String gameName);
-	public abstract long getData(String gameName, String playerName, String fieldName);
+	public abstract long getData(String gameName, BadblockPlayer player, String fieldName);
 	public abstract void calcPoints(String gameName, BadblockPlayer player, RankedCalc calc);
 	
 }
